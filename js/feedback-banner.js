da34b32e2549bfe53910c4e7893718a5a2cc06f9
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2022-05-12 12:59:41 -0600

    Update feedback-banner.js
    comment out external links 404 script for page validation.

--- js/feedback-banner.js
+++ js/feedback-banner.js
@@ -7,14 +7,14 @@ alphaBanner.insertAdjacentHTML('afterbegin', '<section class="experimental alpha
 $('#wb-srch-sub').attr("disabled", true);
 
 //Seperate script to null all canada.ca links to a 404 page (including GC menu) and redirect some in menu
-$( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
-	$("a[href='https://www.canada.ca/en/services/taxes.html']").attr("href", "https://cra-proto.github.io/eol/services/taxes.html").addClass("prototype");
-	$("a[href='https://www.canada.ca/en/services/taxes/income-tax.html']").attr("href", "https://cra-proto.github.io/eol/services/income-tax/index.html").addClass("prototype");
-	$("a[href='https://www.canada.ca/en/services/finance/manage.html']").attr("href", "https://cra-proto.github.io/eol/services/finance/manage.html").addClass("prototype");
-	$(".gcweb-menu a:not(.prototype)").attr("href", "https://cra-proto.github.io/eol/validation/404.html");	
-	$("a[href*='canada.ca']").attr("href", "https://cra-proto.github.io/eol/validation/404.html");
-	$("a[href*='.gc.ca/']").attr("href", "https://cra-proto.github.io/eol/validation/404.html");
-});
+//$( ".gcweb-menu" ).on( "wb-ready.gcweb-menu", function( event ) {
+	//$("a[href='https://www.canada.ca/en/services/taxes.html']").attr("href", "https://cra-proto.github.io/eol/services/taxes.html").addClass("prototype");
+	//$("a[href='https://www.canada.ca/en/services/taxes/income-tax.html']").attr("href", "https://cra-proto.github.io/eol/services/income-tax/index.html").addClass("prototype");
+	//$("a[href='https://www.canada.ca/en/services/finance/manage.html']").attr("href", "https://cra-proto.github.io/eol/services/finance/manage.html").addClass("prototype");
+	//$(".gcweb-menu a:not(.prototype)").attr("href", "https://cra-proto.github.io/eol/validation/404.html");	
+	//$("a[href*='canada.ca']").attr("href", "https://cra-proto.github.io/eol/validation/404.html");
+	//$("a[href*='.gc.ca/']").attr("href", "https://cra-proto.github.io/eol/validation/404.html");
+//});
 
 //Remove visited link design from 404 pages
 var visited_link_styling = "<style> a[href='https://cra-proto.github.io/eol/validation/404.html']:visited{ color:#284162; } </style>"; $('head').append( visited_link_styling );
